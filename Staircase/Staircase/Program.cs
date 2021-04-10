b83a245c0e67bf9a14141fa91d64425af44e78ef
Gino Colaiacovo <gino.colaiacovo@gmail.com>
2021-04-10 12:44:43 -0300

    Added staircase exercise

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Staircase
+{
+    class Program
+    {
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(' ', n - (i + 1));
+                sb.Append('#', i + 1);
+                Console.WriteLine(sb.ToString());
+                sb.Clear();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            staircase(15);
+            Console.ReadLine();
+        }
+    }
+}
