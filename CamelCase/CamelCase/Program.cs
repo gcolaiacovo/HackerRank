c9bdd09899aa2f6636d7bd45831dabe5ce390422
Gino Colaiacovo <gino.colaiacovo@gmail.com>
2021-04-13 21:03:16 -0300

    Agregado ejercicio CamelCase

---
+++ CamelCase/CamelCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(camelcase("saveChangesInTheEditor"));
+            Console.ReadLine();
+        }
+
+        static int camelcase(string s)
+        {
+            int returnValue = 0;
+
+            returnValue = s.Count(n => char.IsUpper(n)) + 1;
+
+            return returnValue;
+        }
+    }
+}
